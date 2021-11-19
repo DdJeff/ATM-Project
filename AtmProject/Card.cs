8da418eb2de12bf64dbecdc403f110e0d40883f8
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-11-19 12:51:41 -0800

    Inital Commit

---
+++ AtmProject/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtmProject
+{
+    public class Card
+    {
+        public string firstname;
+
+        public Card()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+    }
+
+    
+}
